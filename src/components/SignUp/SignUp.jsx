aeaf994a0abcc401745816f84649db51c88ffa78
dev <thakurdev2672@gmail.com>
2024-10-20 23:37:10 +0530

    cssfile

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,40 +1,52 @@
 import React from 'react'
-import  img from './navbar.png'
+import  img from '../../assets/navbar.png'
+import imgPer from '../../assets/personicon.png'
 
 function SignUp() {
   return (
     <>
-    <div className="logo nav"> 
-       <img src={img} alt="picture" />
-    </div>
-    <div className="container flex ">
-    <div className="left">
-        <p className='textleft' >Lorem Ipsum is simply dummy text of the printing and  typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley</p>
+
+    <nav>
+    
+       <img src={img} alt="nav_logo" />
+    
+    </nav>
+
+   <section >
+    <div className="container flex flex-row" >
+    
+    <div className="left w-1/2 flex ">
+        <p className="lefttext" >Lorem Ipsum is simply dummy text of the printing and  typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley</p>
     </div>
     
         
         
-       <div className="right flex justify-center flex-col border-2 rounded-lg">
+       <div className="right   flex justify-center flex-col border-2 border-blue-400 rounded-lg">
         
         <form action="" className='flex flex-col'>
-        <h1 className=' text-center SignUp'>Sign Up</h1>
+        <h1 className=' text-center SignUp font-sans text-xl font-medium'>Sign Up</h1>
         <p className='text-center'>Lorem Ipsum is simply dummy text</p>
-        
-        <input type="name"  className=' m-3 h-11 box-content' placeholder='Name' />
-        <input type="tel" placeholder='Phone no.'className=' m-3 h-11' />
-        <input type="text" placeholder='Company Name'className=' m-3 h-11' />
-        <input type="email" placeholder='Company Email'className=' m-3 h-11' />
-        <input type="number" placeholder='Employee Size'className=' m-3 h-11' />
+        <div className="imagecontainer  relative">
+          <input type="name"  className=' w-full' placeholder='Name' />
+          <img className='personicon absolute' src={imgPer} alt=""/>
+        </div>
+
+        <input type="tel" placeholder='Phone no.'className='' />
+        <input type="text" placeholder='Company Name'className=' m-3 h-9' />
+        <input type="email" placeholder='Company Email'className=' m-3 h-9' />
+        <input type="number" placeholder='Employee Size'className=' m-3 h-9' />
 
         <p className='text-center'>By clicking on proceed you wil accept our </p>
         <p className='text-center'> Terms & Conditions</p>
-        <button type='submit' className='pro-btn'>Proceed</button>
+        <button type='submit' className='pro-btn bg-blue-500 rounded-md'>Proceed</button>
     
-
+ 
         </form>
 
+   
     </div>
     </div>
+    </section>
     
     </>
     
