5ffe9f20e4f2a8c2bfb5b41c378959de0f19bf3c
dev <thakurdev2672@gmail.com>
2024-10-22 17:58:29 +0530

    new assets and interview page dn

--- src/components/Interview/Interview.jsx
+++ src/components/Interview/Interview.jsx
@@ -1,23 +1,22 @@
 import React from 'react'
-import imgCuvette from '../../assets/cuvette.png'
-import imgContact from '../../assets/contact.png'
+
+import imgHome from '../../assets/home.png'
+import LogInNav from '../LogInNav'
+
 
 function Interview() {
   return (
     <>
-    <div className="nav">
-      
-        <img src={imgCuvette} alt="" className='cuvette_img block ' />
-        
-        <div className="contact_text blo">
-            <p>Contact</p>
-        </div>
+    <LogInNav/>
+
     
-    </div>
-    <div className='sec'>
+    <div className='sec relative'>
    
     <div className='horizontal-line'></div>
-    <hr className='vertical-line'/>
+    <img src={imgHome} alt=""  className='absolute top-10' />
+    <div className='vertical-line '></div>
+    <button className='create-btn   bg-blue-500 hover:bg-blue-400  text-white font-sans font-extrabold text-2xl'>Create Interview</button>
+    
     
     </div>
     
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -20,16 +20,16 @@ function SignUp() {
    
     <div className="container  flex flex-row" >
     
-    <div className="left w-1/2 flex ">
+    <div className="left relative w-1/2  ">
         <p className="lefttext" >Lorem Ipsum is simply dummy text of the printing and  typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley</p>
     </div>
        <div className="right  w-1/2 ">
-              <div className='form border-2 border-blue-400 rounded-lg ml-20'>   
-                <form action="/verify" target='' className='flex flex-col '>
+              <div className='form_size border-2 border-blue-400 rounded-lg '>   
+                <form action="/verify" target='' className=''>
                 <h1 className=' text-center SignUp font-sans text-xl font-medium'>Sign Up</h1>
                 <p className='text-center'>Lorem Ipsum is simply dummy text</p>
                 <div className="input-container  relative">
-                  <img className='icon ' src={imgPer} alt=""/>
+                  <img className='icon  ' src={imgPer} alt=""/>
                   <input type="name"  className=' ' placeholder='Name' />
                 </div>
                 <div className="input-container  relative">
